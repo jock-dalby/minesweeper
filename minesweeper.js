ad3ebe7ff5d9148a3bfb34bbaba6151ef51954dd
jock-dalby <jockdalby86@gmail.com>
2016-08-03 10:55:25 +1200

    establish countSurroundingMines function and surroundingMines object prop

--- minesweeper.js
+++ minesweeper.js
@@ -8,223 +8,300 @@ var board = {
     row: 1,
     col: 1,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 1,
     col: 2,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 1,
     col: 3,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 1,
     col: 4,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 1,
     col: 5,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 1,
     col: 6,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 2,
     col: 1,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 2,
     col: 2,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 2,
     col: 3,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 2,
     col: 4,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    // surroundingMines:
   },
   {
     row: 2,
     col: 5,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 2,
     col: 6,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    // surroundingMines:
   },
   {
     row: 3,
     col: 1,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 3,
     col: 2,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 3,
     col: 3,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 3,
     col: 4,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 3,
     col: 5,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 3,
     col: 6,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 4,
     col: 1,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 4,
     col: 2,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 4,
     col: 3,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 4,
     col: 4,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 4,
     col: 5,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 4,
     col: 6,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 5,
     col: 1,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 5,
     col: 2,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    // surroundingMines:
   },
   {
     row: 5,
     col: 3,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 5,
     col: 4,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 5,
     col: 5,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 5,
     col: 6,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    // surroundingMines:
   },
   {
     row: 6,
     col: 1,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 6,
     col: 2,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 6,
     col: 3,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 6,
     col: 4,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 6,
     col: 5,
     isMine: false,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   },
   {
     row: 6,
     col: 6,
     isMine: true,
-    hidden: true
+    isMarked: false,
+    hidden: true,
+    surroundingMines: 0
   }
 ]
 }
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
+  for (var i = 0; i < board.cells.length; i++) {
+    var result = countSurroundingMines(board.cells[i]);
+    board.cells[i].surroundingMines = result;
+    console.log(board.cells[i].surroundingMines);
+  }
   lib.initBoard()
 }
 
@@ -248,4 +325,16 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+  var count = 0;
+  var surroundingCells = [lib.getSurroundingCells(cell.row, cell.col)];
+  for (var i = 0; i < surroundingCells.length; i++) {
+    for (var j = 0; j <surroundingCells[i].length; j++)  {
+    if(surroundingCells[i][j].isMine == true) {
+      count++;
+    } else {
+      count = count;
+    }
+}
+}
+return count;
 }
